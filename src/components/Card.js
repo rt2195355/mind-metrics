9589f1d43456f0b43bec793ed83bf9c83b7f448c
rt2195355 <rt2195355@gmail.com>
2024-07-07 22:47:19 +0530

    fix the icons on cards in home component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,12 @@
 import './card.css'
-import logo from '../assets/mem.png'
+import memory from '../assets/memory.png';
+import reaction from '../assets/reaction.png';
 const Card = ({ name, text, onChange, displayName }) => {
     return (
         <div className="card" onClick={() => {
             onChange(name)
         }}>
-            <img src={logo} alt="" />
+            <img src={displayName === 'Word Memory' ? memory : reaction} alt="" />
             <h1>{displayName}</h1>
             <p>{text}</p>
         </div>
